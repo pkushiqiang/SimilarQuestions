309120732d69553428273d5cb7cb9e857596edb0
pkushiqiang <pkushiqiang@gmail.com>
2014-03-19 21:43:27 -0500

    file to get questions from stackoverflow

---
+++ src/retrievedata/dbclient.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 19 20:55:46 2014
+
+@author: dlmu__000
+"""
+
+from pymongo import MongoClient
+
+class DbClient:
+    
+    def __init__(self, host, port, dbName):
+         self.client = MongoClient(host, port)
+         self.db = self.client[dbName]
+    
+    def getCollection(self, collection_name):
+        return self.db[collection_name]
+         
+ 
