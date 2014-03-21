26d196d98bba03e1935be57d064363ce32b916da
pkushiqiang <pkushiqiang@gmail.com>
2014-03-21 11:59:20 -0500

    fix bugs in get questions

--- src/retrievedata/getQuestions.py
+++ src/retrievedata/getQuestions.py
@@ -6,12 +6,12 @@ Created on Wed Mar 19 20:12:09 2014
 """
 
 import urllib
-import urllib2
 import gzip
 import cStringIO
 import json
 import pymongo
 from dbclient import DbClient 
+import time
 
 
 '''
@@ -40,44 +40,72 @@ class QuestionGetter:
         params['page'] = str(pageNo)
         content = self.makeRequest(params)
         
-        json_data = json.loads(content)
-        items = json_data["items"]
-        return items
+        json_data = json.loads(content)    
+     #   print json_data
+        has_more = json_data["has_more"]
+        print "has_more= " , has_more
         
+        if has_more :
+            if "items" in json_data:            
+                return json_data["items"]
+            else : 
+                return "NO_ITEMS"
+        else:
+            return "NO_MORE"
         
     def makeRequest(self,params):
         param_str = urllib.urlencode(params)  
         url = self.url_pre % param_str
+        retry = True
     #    print 'url =', url
-        response = urllib.urlopen(url)
-        return_data = response.read()
-        s = cStringIO.StringIO(return_data)
-        gz_data = gzip.GzipFile(fileobj=s, mode='rb')
-        content = gz_data.read()
-#        print content
+        while (retry):
+            try : 
+                response = urllib.urlopen(url)
+                return_data = response.read()
+                s = cStringIO.StringIO(return_data)
+                gz_data = gzip.GzipFile(fileobj=s, mode='rb')
+                content = gz_data.read()
+                retry = False
+            except IOError as e:
+                print e
+    #        print content
         return content
         
     def savePage(self,  collection, items ):
+        i = 0
         for question in items:
-            print question["question_id"]
+        #    print question["question_id"]
             question["_id"] = question["question_id"]
             try:
                 collection.insert(question)
+                i+=1
             except pymongo.errors.DuplicateKeyError as e: 
-                print e
-        
+                #print e
+                ()
+        return i
         
 def main():
     pageSize = 100
-    startPageNo = 1
-    endPageNo = 1000
+    startPageNo = 2680
+    endPageNo = 10000
     dbClient = DbClient('localhost', 27017, "SimilarQuestion")
     collection = dbClient.getCollection("question_test")
     
     questionGetter = QuestionGetter(pageSize,"python")
     for  pg in range(startPageNo, endPageNo):
+        print "--- get page %d ---" %pg
         items = questionGetter.getPage(pg)
-        questionGetter.savePage(collection,items)          
 
+     #   print items
+        if ( items == "NO_MORE" ) :
+            print "have no more questions, quit program !!"
+            break
+        
+        print "--- page %d has %d questions ---" %(pg,len(items))
+        if ( items != "NO_ITEMS" ) :
+           i = questionGetter.savePage(collection,items) 
+        print "--- page %d has save %d question " %(pg,i)
+        time.sleep(10)       
+        
 if __name__ == "__main__": 
     main()
