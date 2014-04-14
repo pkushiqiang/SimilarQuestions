684c46e82efd9a2c11cf71dc26a7e0a22a333872
jungin <JungInKoh@dhcp-10-202-146-175.tamulink.tamu.edu>
2014-04-14 17:35:05 -0500

    Merge branch 'master' of https://github.com/pkushiqiang/SimilarQuestions

--- src/experiments/main.py
+++ src/experiments/main.py
@@ -2,8 +2,10 @@ from experimentEngine import ExperimentEngine
 from index import Index
 from vectorizer_TFIDF import Vectorizer_TFIDF
 import json
+import sys
 
 def main():
+    
     questionsFile = open('../../data/python_questions_with_body_abridged.txt')
     questions = []
     
@@ -13,12 +15,25 @@ def main():
     index = Index(questions)
     
     vectorizers = {}
-    tfidf = Vectorizer_TFIDF()
-    vectorizers[tfidf.getName()] = tfidf
-    tfidf = Vectorizer_TFIDF('title')
-    vectorizers[tfidf.getName()] = tfidf
-    tfidf = Vectorizer_TFIDF('body')
-    vectorizers[tfidf.getName()] = tfidf
+    # if we want to call only some of them (from commandline)...
+    if len(sys.argv) > 1:
+        if 'tfidf' in sys.argv:
+            tfidf = Vectorizer_TFIDF()  
+            vectorizers[tfidf.getName()] = tfidf
+        if 'tfidf-title' in sys.argv:
+            tfidf = Vectorizer_TFIDF('title')
+            vectorizers[tfidf.getName()] = tfidf
+        if 'tfidf-body' in sys.argv:
+            tfidf = Vectorizer_TFIDF('body')
+            vectorizers[tfidf.getName()] = tfidf
+    else: # if we want to run all of them
+        tfidf = Vectorizer_TFIDF()
+        vectorizers[tfidf.getName()] = tfidf
+        tfidf = Vectorizer_TFIDF('title')
+        vectorizers[tfidf.getName()] = tfidf
+        tfidf = Vectorizer_TFIDF('body')
+        vectorizers[tfidf.getName()] = tfidf
+        
         
     linkedFile = open('../../data/python_linked.txt')
     linkedDocs = {}
