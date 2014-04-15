48fcc4d5171f84394f51a26ef7cf87efdb2b1bcb
jungin <JungInKoh@Jung-In-Kohui-MacBook-Pro.local>
2014-04-15 07:53:42 -0500

    -s as synonym option

--- src/experiments/main.py
+++ src/experiments/main.py
@@ -11,10 +11,11 @@ def main():
     
     for q in questionsFile:
         questions += [json.loads(q)]
-    
+
     index = Index(questions)
     
     vectorizers = {}
+    synonym = False
     # if we want to call only some of them (from commandline)...
     if len(sys.argv) > 1:
         if 'tfidf' in sys.argv:
@@ -26,6 +27,13 @@ def main():
         if 'tfidf-body' in sys.argv:
             tfidf = Vectorizer_TFIDF('body')
             vectorizers[tfidf.getName()] = tfidf
+        if '-s' in sys.argv:
+            synonym = True
+            if sys.argv[sys.argv.index('-s')+1].isdigit():
+                num_syn = sys.argv[sys.argv.index('-s')+1]
+                print num_syn
+            else:
+                num_syn = 2
     else: # if we want to run all of them
         tfidf = Vectorizer_TFIDF()
         vectorizers[tfidf.getName()] = tfidf
@@ -34,14 +42,13 @@ def main():
         tfidf = Vectorizer_TFIDF('body')
         vectorizers[tfidf.getName()] = tfidf
         
-        
     linkedFile = open('../../data/python_linked.txt')
     linkedDocs = {}
     for q in linkedFile:
         d = json.loads(q)
         linkedDocs[d['qid']] = d['linked']
         
-    #print linkedDocs 
+    #print "linkedDocs", linkedDocs 
     engine = ExperimentEngine(index, linkedDocs, vectorizers)
     
     stats = engine.runExperiments()
