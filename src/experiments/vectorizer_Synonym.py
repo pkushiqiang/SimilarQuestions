76470ee3bb02f5b1ce29a3990887a23b14851cf2
jungin <JungInKoh@dhcp-10-202-147-115.tamulink.tamu.edu>
2014-04-15 19:19:41 -0500

    complete synonym function

--- src/experiments/vectorizer_Synonym.py
+++ src/experiments/vectorizer_Synonym.py
@@ -26,15 +26,44 @@ class Vectorizer_Synonym(Vectorizer):
             postingsList = document.getPostingsList()
             
         vector = {}
+        testingVector={}
         for term in postingsList:
                     
             # this is the ONLY line in this function that is tfidf specific. Most likely, everything
             # else in this function should stay the same for all vectorizers
             vector[term] = postingsList[term].getTFIDF()
-            for ss in wn.synsets(term):
-                for sim in ss.similar_tos():
-                    print sim
-                    #print('    {}'.format(sim))                
+            testingVector[term] = postingsList[term].getTFIDF()
+
+            synonymSet = set()
+            for synset in wn.synsets(term):
+                #print synset.lemma_names
+                for synonym in synset.lemma_names:
+                    if not term == synonym:
+                        synonymSet.add(synonym)
+            #print synonymSet
             
-            
+            synonymList = []
+            scoreList = []
+            additionalTerms = []
+            tmpterms = wn.synsets(term)
+            if len(tmpterms) > 0:
+                term1 = tmpterms[0]
+                for synonym1 in synonymSet:
+                    syn = wn.synsets(synonym1)[0]
+                    sim_score = term1.wup_similarity(syn)
+                    synonymList.append(synonym1)
+                    scoreList.append(sim_score)
+                #print zip(scoreList, synonymList)   
+                additionalTerms = sorted(zip(scoreList, synonymList), reverse=True)
+                additionalTerms = filter(lambda x: x[0]!=None, additionalTerms)
+                additionalTerms = additionalTerms[:self.num_syn]            
+                
+                for score, term2 in additionalTerms:
+                    if term2 not in vector:
+                        score = postingsList[term].getTFIDF() * score
+                        vector[term2] = score
+        print vector
+        print testingVector
+        exit(0)
+                
         return vector
--- test1.py
+++ test1.py
@@ -4,14 +4,44 @@ import nltk
 from nltk.corpus import wordnet as wn
 
 
-#for ss in wn.synsets('python'):
-   #print ss.lemma_names
+#for ss in wn.synsets('sky'):
+   #print "ss", ss.lemma
    #for sim in ss.similar_tos():
-      #sim
+      #print sim
       #print('    {}'.format(sim.name))
 
-vehicle = wn.synset('python.n.01')
-print vehicle
-typesOfVehicles = list(set([w for s in vehicle.closure(lambda s:s.hyponyms()) for w in s.lemma_names]))        
-print typesOfVehicles
-      
+#vehicle = wn.synset('python.n.01')
+#print vehicle.closure
+#typesOfVehicles = list(set([w for s in vehicle.closure(lambda s:s.hyponyms()) for w in s.lemma_names]))        
+#print typesOfVehicles
+if len(wn.synsets('korea')) < 2:
+   print wn.synsets('korea')[0].lemma_names
+
+
+term = 'computer'
+synonymSet = set()
+for synset in wn.synsets(term):
+   print synset.lemma_names
+   for synonym in synset.lemma_names:
+      if not term == synonym:
+         synonymSet.add(synonym)
+print synonymSet
+
+synonymList = []
+scoreList = []
+resultList = []
+term = wn.synsets(term)[0]
+for synonym1 in synonymSet:
+   syn = wn.synsets(synonym1)[0]
+   sim_score = term.wup_similarity(syn)
+   synonymList.append(synonym1)
+   scoreList.append(sim_score)
+print zip(scoreList, synonymList)   
+resultList = sorted(zip(scoreList, synonymList), reverse=True)[:3]
+print resultList
+for x, y in resultList:
+   print x
+   print y
+
+
+   
